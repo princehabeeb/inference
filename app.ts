33b4a57f6c563b7f9b5227fc91623fd2c5581792
princehabeeb <habiy3400@gmail.com>
2023-10-24 23:40:45 +0100

    new

--- app.ts
+++ app.ts
@@ -1,1 +1,17 @@
 let helloWorld = "Hello World";
+
+
+// const user = {
+//     name: "Hayes",
+//     id: 0,
+//   };
+
+  interface User {
+    name: string;
+    id: number;
+  }
+
+  const user: User = {
+    name: "Hayes",
+    id: 0,
+  };
